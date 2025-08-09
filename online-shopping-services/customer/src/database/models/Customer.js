cdc99bba4f85c9440ad752bfb6a47262999a1cb6
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-08-09 10:12:59 +0500

    refactor: update Customer model schema and remove unused Order and Product models

--- online-shopping-services/customer/src/database/models/Customer.js
+++ online-shopping-services/customer/src/database/models/Customer.js
@@ -1,38 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const CustomerSchema = new Schema({
+const CustomerSchema = new Schema(
+  {
     email: String,
     password: String,
     salt: String,
     phone: String,
-    address:[
-        { type: Schema.Types.ObjectId, ref: 'address', require: true }
-    ],
+    address: [{ type: Schema.Types.ObjectId, ref: "address", require: true }],
     cart: [
-        {
-          product: { type: Schema.Types.ObjectId, ref: 'product', require: true},
-          unit: { type: Number, require: true}
-        }
+      {
+        product: {
+          _id: { type: String, require: true },
+          name: { type: String, require: true },
+          price: { type: Number, require: true },
+          banner: { type: String, require: true },
+        },
+        unit: { type: Number, require: true },
+      },
     ],
-    wishlist:[
-        { 
-            type: Schema.Types.ObjectId, ref: 'product', require: true
-        }
+    wishlist: [
+      {
+        _id: { type: String, require: true },
+        name: { type: String, require: true },
+        description: { type: String, require: true },
+        available: { type: Boolean, require: true },
+        price: { type: Number, require: true },
+        banner: { type: String, require: true },
+      },
     ],
-    orders: [ 
-        { type: Schema.Types.ObjectId, ref: 'order', require: true }
-    ]
-},{
+    orders: [
+      {
+        _id: { type: String, require: true },
+        amount: { type: Number, require: true },
+        date: { type: Date, require: true },
+      },
+    ],
+  },
+  {
     toJSON: {
-        transform(doc, ret){
-            delete ret.password;
-            delete ret.salt;
-            delete ret.__v;
-        }
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.salt;
+        delete ret.__v;
+      },
     },
-    timestamps: true
-});
+    timestamps: true,
+  }
+);
 
-module.exports =  mongoose.model('customer', CustomerSchema);
+module.exports = mongoose.model("customer", CustomerSchema);
